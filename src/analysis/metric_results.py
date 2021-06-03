84f2bd4d3511a4bd6fc6062aad2910dad3415053
confusedSerge <serge.kotchourko@gmail.com>
2021-06-03 20:13:43 +0200

    rename

--- src/analysis/metric_results.py
+++ src/analysis/metric_results.py
@@ -11,86 +11,87 @@ class MetricResults():
 
     # ===Util Functions===
     # TODO: Add function?
-    def add_metric(self, name_metric: str, size: tuple, axis_info: tuple = None):
+    def add_metric(self, name_metric: str, size: tuple, axes_info: tuple = None):
         if name_metric in self.metric_dict.keys():
             raise KeyError('Key exist already')
 
         new_metric_matrix = np.full(size, np.NaN)
         self.metric_dict[name_metric] = new_metric_matrix
-        self.metric_info_dict[name_metric] = axis_info
+        self.metric_info_dict[name_metric] = axes_info
 
     def remove_metric(self, name_metric: str):
         return self.metric_dict.pop(name_metric, None)
 
-    def get_values(self, name_metric: str, *axis):
+    def get_values(self, name_metric: str, *axes):
         """
         Gets the values for given point on the axes. 
         The n-th axis value describes the n-th axis.
         >>>args=(None, 1, 1)
             is going to be the same as a[:, 1, 1, ...]
+        args can be of type slice(), int (=position), None (all values along axis), or tuple describing (start, stop [, step]),  
         """
         if name_metric not in self.metric_dict.keys():
             raise KeyError
 
-        assert len(axis) <= len(self.metric_dict[name_metric].shape)
-        if len(axis) == 0:
+        assert len(axes) <= len(self.metric_dict[name_metric].shape)
+        if len(axes) == 0:
             return self.metric_dict[name_metric]
 
-        axis = self._gen_slice_tuple(name_metric, axis)
-        return self.metric_dict[name_metric][axis]
+        axes = self._gen_slice_tuple(name_metric, axes)
+        return self.metric_dict[name_metric][axes]
 
-    def update_value(self, name_metric: str, new_values, *axis):
+    def update_value(self, name_metric: str, new_values, *axes):
         if name_metric not in self.metric_dict.keys():
             raise KeyError
 
-        axis = self._gen_slice_tuple(name_metric, axis)
-        self.metric_dict[name_metric][axis] = new_values
+        axes = self._gen_slice_tuple(name_metric, axes)
+        self.metric_dict[name_metric][axes] = new_values
 
-    def get_axis_info(self, name_metric: str):
+    def get_axes_info(self, name_metric: str):
         if name_metric not in self.metric_info_dict.keys():
             raise KeyError
         return self.metric_info_dict[name_metric]
 
     # ===Eval Functions===
-    def mean(self, name_metric: str, *axis):
-        return np.nanmean(self.get_values(name_metric, *axis))
+    def mean(self, name_metric: str, *axes):
+        return np.nanmean(self.get_values(name_metric, *axes))
     
-    def median(self, name_metric: str, *axis):
-        return np.nanmedian(self.get_values(name_metric, *axis))
+    def median(self, name_metric: str, *axes):
+        return np.nanmedian(self.get_values(name_metric, *axes))
 
-    def std(self, name_metric: str, *axis):
-        return np.nanstd(self.get_values(name_metric, *axis))
+    def std(self, name_metric: str, *axes):
+        return np.nanstd(self.get_values(name_metric, *axes))
     
-    def var(self, name_metric: str, *axis):
-        return np.nanvar(self.get_values(name_metric, *axis))
+    def var(self, name_metric: str, *axes):
+        return np.nanvar(self.get_values(name_metric, *axes))
     
-    def max(self, name_metric: str, *axis):
+    def max(self, name_metric: str, *axes):
         """
         Note, argmax only works, if no NaNs in the given array
         """
-        val = self.get_values(name_metric, *axis)
+        val = self.get_values(name_metric, *axes)
         return np.nanmax(val), np.argmax(val)
 
-    def min(self, name_metric: str, *axis):
+    def min(self, name_metric: str, *axes):
         """
         Note, argmax only works, if no NaNs in the given array
         """
-        val = self.get_values(name_metric, *axis)
+        val = self.get_values(name_metric, *axes)
         return np.nanmin(val), np.argmin(val)
     
     #===Util Functions===
-    def _gen_slice_tuple(self, name_metric: str, axis: tuple):
-        axis = list(axis)
-        for c, v in enumerate(axis):
-            if type(axis[c]) is int:
+    def _gen_slice_tuple(self, name_metric: str, axes: tuple):
+        axes = list(axes)
+        for c, v in enumerate(axes):
+            if type(axes[c]) is int or isinstance(axes[c], slice):
                 pass
-            elif axis[c] == None:
-                axis[c] = slice(self.metric_dict[name_metric].shape[c])
-            elif len(axis[c]) == 2:
-                axis[c] = slice(axis[c][0], axis[c][1])
-            elif len(axis[c]) == 3:
-                axis[c] = slice(axis[c][0], axis[c][1], axis[c][2])
-        return tuple(axis)
+            elif axes[c] == None:
+                axes[c] = slice(self.metric_dict[name_metric].shape[c])
+            elif len(axes[c]) == 2:
+                axes[c] = slice(axes[c][0], axes[c][1])
+            elif len(axes[c]) == 3:
+                axes[c] = slice(axes[c][0], axes[c][1], axes[c][2])
+        return tuple(axes)
 
 if __name__ == "__main__":
     res = MetricResults()
@@ -100,7 +101,7 @@ if __name__ == "__main__":
     res.update_value('randInd', [1, 2], 0, 0)
 
     print(res.get_values('randInd').flatten())
-    print(res.std('randInd', 0, 0))
+    print(res.std('randInd', slice(0, 1), 0))
     print(res.max('randInd', 0, 0))
     print(res.min('randInd', 0, 0))
 
